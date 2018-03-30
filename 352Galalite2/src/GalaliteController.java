b39c91d54e2d4eaefb71ff58497b87807b33c58f
annaholmes <holmesaa@hendrix.edu>
2018-03-29 21:13:21 -0500

    oops! here is everything

---
+++ 352Galalite2/src/GalaliteController.java
@@ -0,0 +1,24 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GalaliteController {
+
+	@FXML
+	private Button start;
+
+	@FXML
+	private Button load;
+
+	@FXML
+	private Button save;
+
+
+	@FXML
+	private void initialize() {
+
+	}
+
+
+
+}
